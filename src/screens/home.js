4ecf7cf91103c934c4b68170f95c7e970f62c8d2
emrediler <facebook61.ts@gmail.com>
2024-03-13 11:04:08 +0300

    update

---
+++ src/screens/home.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import { View, Text, TextInput, TouchableOpacity, Image, TouchableHighlight, SafeAreaView, Keyboard, TouchableWithoutFeedback } from 'react-native';
+import COLORS from "../components/color";
+
+const Home = ()) => {
+    
+}
+
+export default Home;
